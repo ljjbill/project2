ff3770968a9d7c7d8a4d11d6871809f6d5fbdaf8
l-gar <luis.garcia.wi.2793@gmail.com>
2019-03-09 12:30:58 -0600

    updated app.py

---
+++ app.py
@@ -0,0 +1,59 @@
+#################################################
+# IMPORT DEPENDENCIES FOR FLASK
+#################################################
+from flask import (
+   Flask,
+   render_template,
+   jsonify,
+   redirect,
+   make_response,
+   request)
+from flask_cors import CORS
+
+#################################################
+# IMPORT DEPENDENCIES TO ACCESS DB THROUGH SQLALCHEMY FOR DATA INGRESS
+#################################################
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import (
+   create_engine,
+   func,
+   inspect,
+   extract)
+
+from flask_sqlalchemy import SQLAlchemy
+
+#################################################
+# IMPORT DEPENDENCIES FOR OTHER CODE WITHIN ROUTES
+#################################################
+from pandas.util import hash_pandas_object
+import pandas as pd
+import json
+import requests
+import datetime
+from datetime import timedelta
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask (__name__)
+CORS(app)
+
+# Database connection
+app.config['SQLALCHEMY_DATABASE_URI'] =  #"sqlite:///./db.sqlite"
+db = SQLAlchemy(app)
+
+# reflect an existing database into a new model
+Base = automap_base()
+#reflect the tables
+Base.prepare(db.engine, reflect=True)
+
+# Save references to each table
+"""study = Base.classes.study""" # EXAMPLE
+
+# creates a route for the flask front end
+@app.route('/')
+def home():
+   """Return the homepage"""
+   return render_template('index.html') 
